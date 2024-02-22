69c29528bfabb0d85c9b1fe01d3b8ff8655cd1a6
gmsafir <bguillaume@GUILLAUME-P109.efectis.lan>
2024-02-22 18:46:15 +0100

    Debug shell materials

--- gmsafir.py
+++ gmsafir.py
@@ -22,7 +22,7 @@ class Myapp: # Use of class only in order to share 'params' as a global variable
 
         gmsh.initialize(sys.argv)
 
-        self.version="2024-02-06"
+        self.version="2024-02-22"
         self.authors0="Univ. of Liege & Efectis France"
         self.authors="Univ. of Liege"
 
@@ -5977,7 +5977,12 @@ class Myapp: # Use of class only in order to share 'params' as a global variable
                          angles=ast.literal_eval(strangles)
                          #
                          for k in range(len(imattab)):
-                             iglomat=[i0 for i0 in range(len(self.listMats)) if list(self.listMats[i0].keys())[0].split('/')[0]==imattab[k]+";1"][0]
+                             try:
+                                 glomat=[i0 for i0 in range(len(self.listMats)) if list(self.listMats[i0].keys())[0].split('/')[0]==imattab[k]+";2"][0]
+                             except Exception as emsg:
+                                 print(self.listMats)
+                                 gmsh.logger.write("Material "+imattab[k]+"recovered from your .TSH files is not assigned correctly in the current structural case:"+str(emsg), level="error")
+                                 return -1
                              f.write(self.writeLineFortran('(A20,I3)',["REBARMAT",iglomat+1])+"\n")
                              f.write(self.writeLineFortran('(A20,F10.3)',["SECTION",float(isecttab[k])])+"\n")
                              f.write(self.writeLineFortran('(A20,F10.3)',["LEVEL",float(ilevtab[k])])+"\n")
